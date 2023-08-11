07cdb42b5def0a024907956d547544b49ba5ffb1
hkearn777 <howard@kearney.name>
2023-08-11 13:18:40 -0500

    Add project files.

---
+++ Register/Form1.Designer.vb
@@ -0,0 +1,301 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+  Inherits System.Windows.Forms.Form
+
+  'Form overrides dispose to clean up the component list.
+  <System.Diagnostics.DebuggerNonUserCode()> _
+  Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+    Try
+      If disposing AndAlso components IsNot Nothing Then
+        components.Dispose()
+      End If
+    Finally
+      MyBase.Dispose(disposing)
+    End Try
+  End Sub
+
+  'Required by the Windows Form Designer
+  Private components As System.ComponentModel.IContainer
+
+  'NOTE: The following procedure is required by the Windows Form Designer
+  'It can be modified using the Windows Form Designer.  
+  'Do not modify it using the code editor.
+  <System.Diagnostics.DebuggerStepThrough()> _
+  Private Sub InitializeComponent()
+        Dim DataGridViewCellStyle1 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
+        Dim DataGridViewCellStyle2 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.btnClose = New System.Windows.Forms.Button()
+        Me.btnFind = New System.Windows.Forms.Button()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.Column1 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column2 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column3 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column4 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column5 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.btnCancel = New System.Windows.Forms.Button()
+        Me.btnDeposit = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txtDate = New System.Windows.Forms.TextBox()
+        Me.txtDescription = New System.Windows.Forms.TextBox()
+        Me.txtAmount = New System.Windows.Forms.TextBox()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.btnWithdraw = New System.Windows.Forms.Button()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.lblFileName = New System.Windows.Forms.Label()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'btnClose
+        '
+        Me.btnClose.Location = New System.Drawing.Point(673, 566)
+        Me.btnClose.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.btnClose.Name = "btnClose"
+        Me.btnClose.Size = New System.Drawing.Size(84, 35)
+        Me.btnClose.TabIndex = 9
+        Me.btnClose.Text = "Close"
+        Me.btnClose.UseVisualStyleBackColor = True
+        '
+        'btnFind
+        '
+        Me.btnFind.Enabled = False
+        Me.btnFind.Location = New System.Drawing.Point(459, 566)
+        Me.btnFind.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.btnFind.Name = "btnFind"
+        Me.btnFind.Size = New System.Drawing.Size(84, 35)
+        Me.btnFind.TabIndex = 7
+        Me.btnFind.Text = "Find"
+        Me.btnFind.UseVisualStyleBackColor = True
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.AllowUserToAddRows = False
+        Me.DataGridView1.AllowUserToDeleteRows = False
+        DataGridViewCellStyle1.BackColor = System.Drawing.Color.FromArgb(CType(CType(224, Byte), Integer), CType(CType(224, Byte), Integer), CType(CType(224, Byte), Integer))
+        Me.DataGridView1.AlternatingRowsDefaultCellStyle = DataGridViewCellStyle1
+        Me.DataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells
+        Me.DataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.Column1, Me.Column2, Me.Column3, Me.Column4, Me.Column5})
+        DataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft
+        DataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Window
+        DataGridViewCellStyle2.Font = New System.Drawing.Font("Consolas", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        DataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.ControlText
+        DataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight
+        DataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText
+        DataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.[False]
+        Me.DataGridView1.DefaultCellStyle = DataGridViewCellStyle2
+        Me.DataGridView1.Location = New System.Drawing.Point(15, 15)
+        Me.DataGridView1.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.ReadOnly = True
+        Me.DataGridView1.RowHeadersVisible = False
+        Me.DataGridView1.RowHeadersWidth = 62
+        Me.DataGridView1.RowTemplate.Height = 24
+        Me.DataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect
+        Me.DataGridView1.Size = New System.Drawing.Size(1286, 430)
+        Me.DataGridView1.TabIndex = 10
+        Me.DataGridView1.TabStop = False
+        '
+        'Column1
+        '
+        Me.Column1.HeaderText = "Date"
+        Me.Column1.MinimumWidth = 8
+        Me.Column1.Name = "Column1"
+        Me.Column1.ReadOnly = True
+        Me.Column1.Width = 80
+        '
+        'Column2
+        '
+        Me.Column2.HeaderText = "Description"
+        Me.Column2.MaxInputLength = 20
+        Me.Column2.MinimumWidth = 8
+        Me.Column2.Name = "Column2"
+        Me.Column2.ReadOnly = True
+        Me.Column2.Width = 125
+        '
+        'Column3
+        '
+        Me.Column3.HeaderText = "Deposit"
+        Me.Column3.MinimumWidth = 8
+        Me.Column3.Name = "Column3"
+        Me.Column3.ReadOnly = True
+        '
+        'Column4
+        '
+        Me.Column4.HeaderText = "Withdrawal"
+        Me.Column4.MinimumWidth = 8
+        Me.Column4.Name = "Column4"
+        Me.Column4.ReadOnly = True
+        Me.Column4.Width = 123
+        '
+        'Column5
+        '
+        Me.Column5.HeaderText = "BalanceAfter"
+        Me.Column5.MinimumWidth = 8
+        Me.Column5.Name = "Column5"
+        Me.Column5.ReadOnly = True
+        Me.Column5.Width = 138
+        '
+        'btnCancel
+        '
+        Me.btnCancel.Location = New System.Drawing.Point(566, 566)
+        Me.btnCancel.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.btnCancel.Name = "btnCancel"
+        Me.btnCancel.Size = New System.Drawing.Size(84, 35)
+        Me.btnCancel.TabIndex = 8
+        Me.btnCancel.Text = "Cancel"
+        Me.btnCancel.UseVisualStyleBackColor = True
+        '
+        'btnDeposit
+        '
+        Me.btnDeposit.Location = New System.Drawing.Point(641, 502)
+        Me.btnDeposit.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.btnDeposit.Name = "btnDeposit"
+        Me.btnDeposit.Size = New System.Drawing.Size(116, 39)
+        Me.btnDeposit.TabIndex = 5
+        Me.btnDeposit.Text = "Deposit"
+        Me.btnDeposit.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(24, 468)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(44, 20)
+        Me.Label1.TabIndex = 7
+        Me.Label1.Text = "Date"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(196, 468)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(89, 20)
+        Me.Label2.TabIndex = 8
+        Me.Label2.Text = "Description"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(288, 510)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(65, 20)
+        Me.Label3.TabIndex = 9
+        Me.Label3.Text = "Amount"
+        '
+        'txtDate
+        '
+        Me.txtDate.Location = New System.Drawing.Point(73, 465)
+        Me.txtDate.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.txtDate.Name = "txtDate"
+        Me.txtDate.Size = New System.Drawing.Size(112, 26)
+        Me.txtDate.TabIndex = 1
+        '
+        'txtDescription
+        '
+        Me.txtDescription.Location = New System.Drawing.Point(291, 468)
+        Me.txtDescription.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.txtDescription.Name = "txtDescription"
+        Me.txtDescription.Size = New System.Drawing.Size(466, 26)
+        Me.txtDescription.TabIndex = 2
+        '
+        'txtAmount
+        '
+        Me.txtAmount.Location = New System.Drawing.Point(358, 510)
+        Me.txtAmount.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.txtAmount.Name = "txtAmount"
+        Me.txtAmount.Size = New System.Drawing.Size(112, 26)
+        Me.txtAmount.TabIndex = 3
+        '
+        'btnClear
+        '
+        Me.btnClear.Location = New System.Drawing.Point(73, 499)
+        Me.btnClear.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(84, 39)
+        Me.btnClear.TabIndex = 6
+        Me.btnClear.Text = "Clear"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'btnWithdraw
+        '
+        Me.btnWithdraw.Location = New System.Drawing.Point(496, 502)
+        Me.btnWithdraw.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.btnWithdraw.Name = "btnWithdraw"
+        Me.btnWithdraw.Size = New System.Drawing.Size(125, 39)
+        Me.btnWithdraw.TabIndex = 4
+        Me.btnWithdraw.Text = "Withdrawal"
+        Me.btnWithdraw.UseVisualStyleBackColor = True
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(9, 620)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(80, 20)
+        Me.Label4.TabIndex = 17
+        Me.Label4.Text = "File Stats:"
+        '
+        'lblFileName
+        '
+        Me.lblFileName.AutoSize = True
+        Me.lblFileName.Location = New System.Drawing.Point(96, 620)
+        Me.lblFileName.Name = "lblFileName"
+        Me.lblFileName.Size = New System.Drawing.Size(121, 20)
+        Me.lblFileName.TabIndex = 18
+        Me.lblFileName.Text = "RegisterFile.csv"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1313, 649)
+        Me.Controls.Add(Me.lblFileName)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.btnWithdraw)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.txtAmount)
+        Me.Controls.Add(Me.txtDescription)
+        Me.Controls.Add(Me.txtDate)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.btnDeposit)
+        Me.Controls.Add(Me.btnCancel)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.btnFind)
+        Me.Controls.Add(Me.btnClose)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.Name = "Form1"
+        Me.Text = "Register"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnClose As Button
+  Friend WithEvents btnFind As Button
+  Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents btnCancel As Button
+    Friend WithEvents btnDeposit As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents txtDate As TextBox
+    Friend WithEvents txtDescription As TextBox
+    Friend WithEvents txtAmount As TextBox
+    Friend WithEvents btnClear As Button
+    Friend WithEvents btnWithdraw As Button
+    Friend WithEvents Label4 As Label
+    Friend WithEvents lblFileName As Label
+    Friend WithEvents Column1 As DataGridViewTextBoxColumn
+    Friend WithEvents Column2 As DataGridViewTextBoxColumn
+    Friend WithEvents Column3 As DataGridViewTextBoxColumn
+    Friend WithEvents Column4 As DataGridViewTextBoxColumn
+    Friend WithEvents Column5 As DataGridViewTextBoxColumn
+End Class
---
+++ Register/Form1.vb
@@ -0,0 +1,254 @@
+﻿Public Class Form1
+  'Register Program. 
+  'Author. Howard Kearney
+  'Date-Written. November 2017.
+  'Description. Simplied checkbook register for my wife.
+  'Change-History.
+  ' 8/11/2023 hk v3 - display full path/filename of the csv data file
+  ' 7/7/2018  hk v2 - Position DataGridView to last record on file
+  '                 - Correct Tabbing positions
+  '                 - Widen Screen so Scroll bar always shows
+  '                 - Have csv data file reside in the UserProfile\Documents folder
+  ' 11/1/2017 HK v1 New Code
+  '----------------------------------------------------------------------------------------------------------
+  Dim RegisterFile As String = "register.csv"
+  Dim ProgramVersion As String = "v3"
+  Dim AddedANewRow As Boolean = False
+    Public Const MAKINGDEPOSIT As Short = 1
+    Public Const MAKINGWITHDRAWAL As Short = 2
+
+  Private Sub btnClose_Click(sender As Object, e As EventArgs) Handles btnClose.Click
+        'before closing save the register file.
+        Dim fName As String = Environment.GetEnvironmentVariable("UserProfile") & "\Documents\" & RegisterFile
+        Dim cellvalue As String = ""
+        Dim rowLine As String = ""
+        Dim objWriter As New System.IO.StreamWriter(fName, False)
+        Dim Delim As String = ","
+        Dim LastRow As Integer = DataGridView1.Rows.Count - 1
+        Dim LastCol As Integer = DataGridView1.Columns.Count - 1
+        Dim Col As Integer = 0
+        Dim Row As Integer = 0
+
+        Try
+
+      For Row = 0 To LastRow
+        For Col = 0 To LastCol
+          If Not TypeOf DataGridView1.CurrentRow.Cells.Item(Col).Value Is DBNull Then
+            cellvalue = DataGridView1.Item(Col, Row).Value
+            If Col > 1 Then 'format amount fields by removing leadings spaces and any commas
+              cellvalue = LTrim(Str(Val(cellvalue)))
+            End If
+          Else
+            cellvalue = ""
+          End If
+          'rowLine = rowLine + cellvalue
+          ' if this is last field; do not add delimiter
+          If Col = LastCol Then
+            rowLine = rowLine + cellvalue
+          Else
+            rowLine = rowLine + cellvalue + Delim
+          End If
+        Next
+        objWriter.WriteLine(rowLine)
+        rowLine = ""
+      Next
+
+      objWriter.Close()
+      MsgBox("Register saved, it's good to be saved.")
+
+    Catch exc As Exception
+
+      MessageBox.Show("Error occured while writing to the register." + exc.ToString())
+
+    Finally
+      FileClose(1)
+
+    End Try
+
+    Close()
+  End Sub
+
+
+  Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+    Dim fName As String = Environment.GetEnvironmentVariable("UserProfile") & "\Documents\" & RegisterFile
+    Dim RowNum As Integer = 0
+    Dim NumberOfFields As Integer = 0
+    Dim TextLine As String = ""
+    Dim SplitLine() As String
+    Dim PrevBalance As Long = 0
+    Dim DepositAmount As Decimal = 0
+    Dim WithdrawalAmount As Decimal = 0
+    Dim NewBalanceAmount As Decimal = 0
+
+    Text = "Register " & ProgramVersion     'puts version value onto the form title.
+
+    If System.IO.File.Exists(fName) = True Then
+      Dim objReader As New System.IO.StreamReader(fName)
+      Do While objReader.Peek() <> -1
+        RowNum = RowNum + 1
+        TextLine = objReader.ReadLine()
+        SplitLine = Split(TextLine, ",")
+        NumberOfFields = SplitLine.Count
+        If NumberOfFields <> 5 Then
+          MsgBox("CSV must hold only 5 fields. row#" & RowNum, MsgBoxStyle.OkOnly, "Error")
+          End
+        End If
+        If Val(SplitLine(2)) = 0 Then
+          SplitLine(2) = ""
+        End If
+        If Val(SplitLine(2)) > 0 Then
+          DepositAmount = Val(SplitLine(2))
+          SplitLine(2) = FormatRegisterAmount(DepositAmount)
+        End If
+        If Val(SplitLine(3)) = 0 Then
+          SplitLine(3) = ""
+        End If
+        If Val(SplitLine(3)) > 0 Then
+          WithdrawalAmount = Val(SplitLine(3))
+          SplitLine(3) = FormatRegisterAmount(WithdrawalAmount)
+        End If
+        NewBalanceAmount = Val(SplitLine(4))
+        SplitLine(4) = FormatRegisterAmount(NewBalanceAmount)
+
+        DataGridView1.Rows.Add(SplitLine)
+      Loop
+      objReader.Close()
+    Else
+      MsgBox("File " & fName & "Does Not Exist." & vbCrLf & "New file will be created at Close.")
+    End If
+
+    lblFileName.Text = fName & ", #Records " & DataGridView1.Rows.Count
+
+    Call PositionToLastRow()
+
+  End Sub
+
+    Private Sub PositionToLastRow()
+        Dim LastRow As Integer = DataGridView1.Rows.Count
+        If LastRow > 0 Then
+            DataGridView1.CurrentCell = DataGridView1.Rows(LastRow - 1).Cells(0)
+        End If
+    End Sub
+
+    Private Sub btnCancel_Click(sender As Object, e As EventArgs) Handles btnCancel.Click
+        If MsgBox("Are you sure you want to Cancel", MsgBoxStyle.OkCancel, "Confirm Cancel Request") = vbOK Then
+            Close()
+        End If
+    End Sub
+
+
+    Private Sub btnDeposit_Click(sender As Object, e As EventArgs) Handles btnDeposit.Click
+        If TextFieldsAreOkay() = True Then
+            Deposit(ComputeNewBalance(MAKINGDEPOSIT, ComputePreviousBalance()))
+            ClearTheTextFields()
+            PositionToLastRow()
+        End If
+    End Sub
+
+  Private Sub btnWithdraw_Click(sender As Object, e As EventArgs) Handles btnWithdraw.Click
+        If TextFieldsAreOkay() = True Then
+            Withdrawal(ComputeNewBalance(MAKINGWITHDRAWAL, ComputePreviousBalance()))
+            ClearTheTextFields()
+            PositionToLastRow()
+        End If
+    End Sub
+
+  Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+    ClearTheTextFields()
+  End Sub
+
+  Private Sub ClearTheTextFields()
+    txtDate.Text = ""
+    txtDescription.Text = ""
+    txtAmount.Text = ""
+  End Sub
+
+  Function TextFieldsAreOkay() As Boolean
+        'validate text fields are okay
+        If txtDate.TextLength = 0 Then
+            MsgBox("Date must be entered.", MsgBoxStyle.OkOnly, "Error")
+            Return False
+        End If
+        If Not IsDate(txtDate.Text) Then
+            MsgBox("Date format invalid.", MsgBoxStyle.OkOnly, "Error")
+            Return False
+        End If
+    If txtDescription.TextLength = 0 Then
+      MsgBox("Description must be entered.", MsgBoxStyle.OkOnly, "Error")
+      Return False
+    End If
+    If txtDescription.TextLength > 20 Then
+      MsgBox("Description length must be <= 20 characters", MsgBoxStyle.OkOnly, "Error")
+      Return False
+    End If
+    If txtDescription.Text.IndexOf(",") <> -1 Then
+            MsgBox("Description must not have a COMMA.", MsgBoxStyle.OkOnly, "Error")
+            Return False
+        End If
+        If txtAmount.TextLength = 0 Then
+            MsgBox("Amount must be entered.", MsgBoxStyle.OkOnly, "Error")
+            Return False
+        End If
+        Return True
+  End Function
+
+  Private Sub Withdrawal(NewBalance As Decimal)
+    Dim row As String() = New String() _
+       {txtDate.Text,
+        txtDescription.Text,
+        "",
+        FormatRegisterAmount(txtAmount.Text),
+        FormatRegisterAmount(LTrim(Str(NewBalance)))}
+    DataGridView1.Rows.Add(row)
+  End Sub
+
+  Private Sub Deposit(NewBalance As Decimal)
+    Dim row As String() = New String() _
+       {txtDate.Text,
+        txtDescription.Text,
+        FormatRegisterAmount(txtAmount.Text),
+        "",
+        FormatRegisterAmount(LTrim(Str(NewBalance)))}
+    DataGridView1.Rows.Add(row)
+  End Sub
+
+  Function FormatRegisterAmount(Amount As Decimal) As String
+    Return String.Format("{0,10:N2}", Amount)
+  End Function
+
+  Function ComputePreviousBalance() As Decimal
+    Dim row As Integer = 0
+    Dim PreviousBalance As Decimal = 0.0
+    row = DataGridView1.Rows.Count - 1
+    If row < 0 Then 'there is no previous row
+      Return 0
+    End If
+    PreviousBalance = Val(DataGridView1.Rows(row).Cells(4).Value)
+    'MsgBox("PrevBal=" & PreviousBalance, MsgBoxStyle.OkOnly, "debug")
+    Return PreviousBalance
+  End Function
+
+  Function ComputeNewBalance(ButtonPressed As Short, PreviousBalance As Decimal) As Decimal
+    Dim Amount As Decimal = 0
+
+    Dim NewBalance As Decimal = 0
+
+    ' Compute the New Balance
+    Amount = Val(txtAmount.Text)
+    Select Case ButtonPressed
+      Case MAKINGDEPOSIT
+        NewBalance = PreviousBalance + Amount
+      Case MAKINGWITHDRAWAL
+        NewBalance = PreviousBalance - Amount
+      Case Else
+        MsgBox("Unsupported Feature", MsgBoxStyle.OkOnly, "Internal Error")
+        End
+    End Select
+    'MsgBox("NewBal=" & NewBalance & ":Amount=" & Amount, MsgBoxStyle.OkOnly, "debug")
+    Return NewBalance
+  End Function
+
+    Private Sub btnFind_Click(sender As Object, e As EventArgs) Handles btnFind.Click
+
+    End Sub
+End Class
